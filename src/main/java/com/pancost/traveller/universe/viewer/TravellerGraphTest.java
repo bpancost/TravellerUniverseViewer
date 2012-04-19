b1a142fbd27fca6f3fe86ed251b0ea3364ddadb0
Brandon Pancost <qweedom@gmail.com>
2012-04-19 02:38:05 -0700

    Added the a search depth counter.

--- src/main/java/com/pancost/traveller/universe/viewer/TravellerGraphTest.java
+++ src/main/java/com/pancost/traveller/universe/viewer/TravellerGraphTest.java
@@ -33,41 +33,39 @@ import javax.swing.JFrame;
  */
 public class TravellerGraphTest extends JFrame {
     
-    ArrayList<Planet> planetList = new ArrayList<>();
-    TransactionalGraph graphDB;
-    Graph<Vertex,Edge> visGraph;
-    DefaultVisualizationModel<Vertex,Edge> visModel;
-    VisualizationViewer<Vertex,Edge> visServer;
-    Layout<Vertex,Edge> layout;
-    FramesManager framesManager;
+    private static final int MAX_SHIFT_DEPTH = 5;
+    private static final int DISPLAY_WIDTH = 1680;
+    private static final int DISPLAY_HEIGHT = 1000;
+    
+    private ArrayList<Planet> planetList = new ArrayList<>();
+    private TransactionalGraph graphDB;
+    private Graph<Vertex,Edge> visGraph;
+    private DefaultVisualizationModel<Vertex,Edge> visModel;
+    private VisualizationViewer<Vertex,Edge> visServer;
+    private Layout<Vertex,Edge> layout;
+    private FramesManager framesManager;
 
     public TravellerGraphTest() {
         graphDB = new Neo4jGraph("C:/traveller/graphdb");
         framesManager = new FramesManager(graphDB);
-        visGraph = new SparseMultigraph<>();
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
         Iterable<PlanetList> planetListIterable = framesManager.frameVertices(Index.VERTICES, "indexed", "YES", PlanetList.class);
         planetList = new ArrayList<>(planetListIterable.iterator().next().getPlanetList());
         
         Planet planet = planetList.get(0);
-        Vertex planetVertex = planet.asVertex();
-        visGraph.addVertex(planetVertex);
         
-        Collection<Shift> shifts = planet.getShifts();
-        for(Shift shift : shifts){
-            Planet otherPlanet = shift.getToPlanet();
-            visGraph.addVertex(otherPlanet.asVertex());
-            visGraph.addEdge(shift.asEdge(), planetVertex, otherPlanet.asVertex());
-        }
+        visGraph = new SparseMultigraph<>();
+        visGraph.addVertex(planet.asVertex());
         
         layout = new FRLayout(visGraph);
-        layout.setSize(new Dimension(800,800));
         
         visModel = new DefaultVisualizationModel<>(layout);
         
         visServer = new VisualizationViewer<>(visModel);
-        visServer.setPreferredSize(new Dimension(800,800));
+        visServer.setPreferredSize(new Dimension(DISPLAY_WIDTH,DISPLAY_HEIGHT));
+        
+        addPlanetShiftsToVisServer(planet);
         
         visServer.getRenderContext().setVertexLabelTransformer(new PlanetLabeller(framesManager));
         visServer.getRenderer().getVertexLabelRenderer().setPosition(Position.N);
@@ -83,18 +81,8 @@ public class TravellerGraphTest extends JFrame {
 
             @Override
             public void graphClicked(Vertex v, MouseEvent me) {
-                visGraph = new SparseMultigraph<>();
-                visGraph.addVertex(v);
                 Planet planet = framesManager.frame(v, Planet.class);
-                Collection<Shift> shifts = planet.getShifts();
-                for(Shift shift : shifts){
-                    Planet otherPlanet = shift.getToPlanet();
-                    visGraph.addVertex(otherPlanet.asVertex());
-                    visGraph.addEdge(shift.asEdge(), v, otherPlanet.asVertex());
-                }
-                
-                setLayout(new FRLayout(visGraph));
-                visModel.setGraphLayout(layout);
+                addPlanetShiftsToVisServer(planet);
                 /*Layout<Node,Relationship> newLayout = new FRLayout(visGraph);
                 //StaticLayout<Node,Relationship> staticLayout = new StaticLayout<Node,Relationship>(visGraph, newLayout);
                 LayoutTransition<Node,Relationship> transition = new LayoutTransition<Node,Relationship>(visServer, visServer.getGraphLayout(), newLayout);
@@ -105,18 +93,44 @@ public class TravellerGraphTest extends JFrame {
 
             @Override
             public void graphPressed(Vertex v, MouseEvent me) {
-                //throw new UnsupportedOperationException("Not supported yet.");
             }
 
             @Override
             public void graphReleased(Vertex v, MouseEvent me) {
-                //throw new UnsupportedOperationException("Not supported yet.");
             }
         
         });
         
-        this.setSize(new Dimension(800,800));
+        this.setTitle("Traveller Graph Tester");
+        this.setSize(new Dimension(DISPLAY_WIDTH,DISPLAY_HEIGHT));
         add(visServer);
+    }
+    
+    private void addPlanetShiftsToVisServer(Planet planet){
+        visGraph = new SparseMultigraph<>();
+        
+        addPlanetShiftsToVisRecursive(planet, MAX_SHIFT_DEPTH);
+        
+        setLayout(new FRLayout(visGraph));
+        layout.setSize(new Dimension(DISPLAY_WIDTH-40,DISPLAY_HEIGHT-40));
+        visModel.setGraphLayout(layout);
+    }
+    
+    private void addPlanetShiftsToVisRecursive(Planet planet, int remainingDepth){
+        if(!visGraph.containsVertex(planet.asVertex())){
+            visGraph.addVertex(planet.asVertex());
+        }
+        Collection<Shift> shifts = planet.getShifts();
+        for(Shift shift : shifts){
+            Planet otherPlanet = shift.getToPlanet();
+            if(!visGraph.containsVertex(otherPlanet.asVertex())){
+                visGraph.addVertex(otherPlanet.asVertex());
+                visGraph.addEdge(shift.asEdge(), planet.asVertex(), otherPlanet.asVertex());
+                if(remainingDepth > 1){
+                    addPlanetShiftsToVisRecursive(otherPlanet, remainingDepth-1);
+                }
+            }
+        }
     }
     
     public void setLayout(Layout<Vertex,Edge> layout){
